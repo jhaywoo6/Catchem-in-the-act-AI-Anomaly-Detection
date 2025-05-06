9494b2abe384b17b395516ee3cae2ce5d251b4c4
Luke Cashman <lcashman@uncc.edu>
2025-05-06 11:17:35 -0400

    Add files via upload

---
+++ GRU_PoseLift/models/training.py
@@ -0,0 +1,197 @@
+import os
+import time
+import shutil
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+def adjust_lr(optimizer, epoch, lr=None, lr_decay=None, scheduler=None):
+    if scheduler is not None:
+        scheduler.step()
+        new_lr = scheduler.get_last_lr()[0]
+    elif (lr is not None) and (lr_decay is not None):
+        new_lr = lr * (lr_decay ** epoch)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = new_lr
+    else:
+        raise ValueError('Missing parameters for LR adjustment')
+    return new_lr
+
+
+def compute_loss(nll, reduction="mean", mean=0):
+    if reduction == "mean":
+        losses = {"nll": torch.mean(nll)}
+    elif reduction == "logsumexp":
+        losses = {"nll": torch.logsumexp(nll, dim=0)}
+    elif reduction == "exp":
+        losses = {"nll": torch.exp(torch.mean(nll) - mean)}
+    elif reduction == "none":
+        losses = {"nll": nll}
+
+    losses["total_loss"] = losses["nll"]
+    return losses
+
+
+class GRUModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim=1, num_layers=2, dropout=0.3, bidirectional=True):
+        super(GRUModel, self).__init__()
+        self.bidirectional = bidirectional
+        self.num_directions = 2 if bidirectional else 1
+        self.hidden_dim = hidden_dim
+
+        self.gru = nn.GRU(
+            input_dim,
+            hidden_dim,
+            num_layers,
+            batch_first=True,
+            dropout=dropout if num_layers > 1 else 0,
+            bidirectional=bidirectional
+        )
+
+        self.attn = nn.Sequential(
+            nn.Linear(hidden_dim * self.num_directions, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, 1)
+        )
+
+        self.norm = nn.LayerNorm(hidden_dim * self.num_directions)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim * self.num_directions, output_dim)
+        self.loss_fn = nn.BCEWithLogitsLoss(reduction='none')
+
+    def forward(self, x, label=None, score=None):
+        gru_out, _ = self.gru(x)  # [batch, seq_len, hidden_dim*num_directions]
+
+        # Attention over time
+        attn_weights = torch.softmax(self.attn(gru_out).squeeze(-1), dim=1)  # [batch, seq_len]
+        attn_output = torch.sum(gru_out * attn_weights.unsqueeze(-1), dim=1)  # [batch, hidden_dim*num_directions]
+
+        attn_output = self.norm(attn_output)
+        attn_output = self.dropout(attn_output)
+        logits = self.fc(attn_output).squeeze(-1)
+
+        nll = self.loss_fn(logits, label.float()) if label is not None else None
+        return logits, nll
+
+    def set_actnorm_init(self):
+        for name, module in self.named_modules():
+            if isinstance(module, nn.Linear):
+                nn.init.kaiming_normal_(module.weight)
+
+
+class Trainer:
+    def __init__(self, args, model, train_loader, test_loader,
+                 optimizer_f=None, scheduler_f=None):
+        self.model = model
+        self.args = args
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        if optimizer_f is None:
+            self.optimizer = self.get_optimizer()
+        else:
+            self.optimizer = optimizer_f(self.model.parameters())
+        self.scheduler = scheduler_f(self.optimizer) if scheduler_f else None
+
+    def get_optimizer(self):
+        if self.args.optimizer == 'adam':
+            return optim.Adam(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
+        elif self.args.optimizer == 'adamx':
+            return optim.Adamax(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
+        return optim.SGD(self.model.parameters(), lr=self.args.lr)
+
+    def adjust_lr(self, epoch):
+        return adjust_lr(self.optimizer, epoch, self.args.model_lr, self.args.model_lr_decay, self.scheduler)
+
+    def save_checkpoint(self, epoch, is_best=False, filename=None):
+        state = self.gen_checkpoint_state(epoch)
+        if filename is None:
+            filename = 'checkpoint.pth.tar'
+        state['args'] = self.args
+        path_join = os.path.join(self.args.ckpt_dir, filename)
+        torch.save(state, path_join)
+        if is_best:
+            shutil.copy(path_join, os.path.join(self.args.ckpt_dir, 'checkpoint_best.pth.tar'))
+
+    def load_checkpoint(self, filename):
+        try:
+            checkpoint = torch.load(filename)
+            self.start_epoch = checkpoint['epoch']
+            self.model.load_state_dict(checkpoint['state_dict'], strict=False)
+            self.model.set_actnorm_init()
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("Checkpoint loaded successfully from '{}' at (epoch {})\n"
+                  .format(filename, checkpoint['epoch']))
+        except FileNotFoundError:
+            print("No checkpoint exists from '{}'. Skipping...\n".format(self.args.ckpt_dir))
+
+    def train(self, log_writer=None, clip=100):
+        time_str = time.strftime("%b%d_%H%M_")
+        checkpoint_filename = time_str + '_checkpoint.pth.tar'
+        start_epoch = 0
+        num_epochs = self.args.epochs
+        self.model.train().to(self.args.device)
+        key_break = False
+        for epoch in range(start_epoch, num_epochs):
+            if key_break:
+                break
+            print("Starting Epoch {} / {}".format(epoch + 1, num_epochs))
+            pbar = tqdm(self.train_loader)
+            for itern, data_arr in enumerate(pbar):
+                try:
+                    data = [data.to(self.args.device, non_blocking=True) for data in data_arr]
+                    score = data[-2].amin(dim=-1)
+                    label = data[-1]
+                    samp = data[0] if self.args.model_confidence else data[0][:, :self.args.input_dim]
+                    z, nll = self.model(samp.float(), label=label, score=score)
+                    if nll is None:
+                        continue
+                    if self.args.model_confidence:
+                        nll = nll * score
+                    losses = compute_loss(nll, reduction="mean")["total_loss"]
+                    losses.backward()
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), clip)
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
+                    pbar.set_description("Loss: {:.4f}".format(losses.item()))
+                    if log_writer:
+                        log_writer.add_scalar('NLL Loss', losses.item(), epoch * len(self.train_loader) + itern)
+
+                except KeyboardInterrupt:
+                    print('Keyboard Interrupted. Save results? [yes/no]')
+                    choice = input().lower()
+                    if choice == "yes":
+                        key_break = True
+                        break
+                    else:
+                        exit(1)
+
+            self.save_checkpoint(epoch, filename=checkpoint_filename)
+            new_lr = self.adjust_lr(epoch)
+            print('Checkpoint Saved. New LR: {:.3e}'.format(new_lr))
+
+    def test(self):
+        self.model.eval().to(self.args.device)
+        pbar = tqdm(self.test_loader)
+        probs = torch.empty(0).to(self.args.device)
+        print("Starting Test Eval")
+        for itern, data_arr in enumerate(pbar):
+            data = [data.to(self.args.device, non_blocking=True) for data in data_arr]
+            score = data[-2].amin(dim=-1)
+            samp = data[0] if self.args.model_confidence else data[0][:, :self.args.input_dim]
+            with torch.no_grad():
+                z, nll = self.model(samp.float(), label=torch.ones(data[0].shape[0]), score=score)
+            if self.args.model_confidence:
+                nll = nll * score
+            probs = torch.cat((probs, -1 * nll), dim=0)
+        return probs.cpu().detach().numpy().squeeze().copy(order='C')
+
+    def gen_checkpoint_state(self, epoch):
+        return {
+            'epoch': epoch + 1,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        }
+
